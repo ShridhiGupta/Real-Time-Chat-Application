2c73081bd40083383f67b32d0581f14de6dd189e
Shridhi Gupta <149915524+ShridhiGupta@users.noreply.github.com>
2026-08-20 08:15:57 +0530

    Update ChatController.java

--- Code/src/main/java/com/chat/app/controller/ChatController.java
+++ Code/src/main/java/com/chat/app/controller/ChatController.java
@@ -1,6 +1,5 @@
 package com.chat.app.controller;
 
-
 import com.chat.app.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
